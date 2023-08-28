efaf31425075557396eab437cf97887de56f29be
Joao Victor Negreiros <joaovictornsv@gmail.com>
2023-08-28 20:56:39 -0300

    fix: break words in Task

--- src/atomic/atoms/Checkbox.jsx
+++ src/atomic/atoms/Checkbox.jsx
@@ -1,6 +1,3 @@
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faCheck} from "@fortawesome/free-solid-svg-icons/faCheck";
-
 export const Checkbox = ({
   label,
   id,
@@ -8,25 +5,17 @@ export const Checkbox = ({
   ...rest
 }) => {
   return (
-    <label htmlFor={id} className="flex justify-start items-start text-sm cursor-pointer gap-2">
+    <label htmlFor={id} className="flex min-w-0 w-full justify-start items-start text-sm cursor-pointer gap-2">
       <input
         type="checkbox"
-        className="hidden p-2 rounded"
+        className="mt-1 rounded bg-neutral-600 accent-zinc-200"
         id={id}
         {...rest}
       />
 
-      <div>
-        <div className={`h-4 w-4 rounded-sm ${checked?'bg-zinc-200':'bg-zinc-600 '} flex items-center justify-center`}>
-          <FontAwesomeIcon icon={faCheck} className={`text-zinc-800 text-xs ${checked?'block':'hidden'}`}/>
-        </div>
-      </div>
-
-      <div>
-        <span className={`line-clamp-3 break-all ${checked?'line-through':''}`}>
-          {label}
-        </span>
-      </div>
+      <span className={`line-clamp-3 break-words ${checked?'line-through':''}`}>
+        {label}
+      </span>
     </label>
   )
 }
