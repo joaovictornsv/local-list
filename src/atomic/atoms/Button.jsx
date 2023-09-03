0e8c7cfc131134a2d700ad7c74abe8c2148144a2
Joao Victor Negreiros <joaovictornsv@gmail.com>
2023-09-02 22:02:33 -0300

    - remove focus outline css

--- src/atomic/atoms/Button.jsx
+++ src/atomic/atoms/Button.jsx
@@ -13,11 +13,11 @@ const ICON_SIZES = {
 }
 
 const BUTTON_TYPES = {
-  primary: 'text-zinc-900 bg-zinc-200 border-none hover:bg-zinc-300 focus:bg-zinc-200 transition-colors duration-75 focus:outline-none focus:ring-1 focus:ring-zinc-300',
-  secondary: 'text-zinc-200 bg-zinc-950 border-none hover:bg-zinc-900 focus:bg-zinc-800 transition-colors duration-75 focus:outline-none focus:ring-1 focus:ring-zinc-400',
-  ghost: 'text-zinc-200 border-none bg-transparent focus:bg-zinc-800 focus:outline-none transition-colors duration-75 hover:bg-zinc-900',
-  outline: 'text-zinc-200 border-zinc-700 bg-transparent focus:bg-zinc-800 focus:outline-none transition-colors duration-75 hover:bg-zinc-800 hover:border-zinc-600',
-  danger: 'text-zinc-200 border-none bg-red-600 focus:bg-red-500 focus:outline-none transition-colors duration-75 hover:bg-red-700 focus:ring-1 focus:ring-red-300'
+  primary: 'text-zinc-900 bg-zinc-200 border-none hover:bg-zinc-300 transition-colors duration-75 focus:outline-none',
+  secondary: 'text-zinc-200 bg-zinc-950 border-none hover:bg-zinc-900 transition-colors duration-75 focus:outline-none',
+  ghost: 'text-zinc-200 border-none bg-transparent focus:outline-none transition-colors duration-75 hover:bg-zinc-900',
+  outline: 'text-zinc-200 border-zinc-700 bg-transparent focus:outline-none transition-colors duration-75 hover:bg-zinc-800 hover:border-zinc-600',
+  danger: 'text-zinc-200 border-none bg-red-600 focus:outline-none transition-colors duration-75 hover:bg-red-700'
 }
 
 // eslint-disable-next-line react/display-name
